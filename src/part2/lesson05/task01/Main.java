cd2bff66bfde904a7488bd51964bc70eabb96f0c
ZaglubotskiyMN <zaglubotskii@gmail.com>
2021-01-15 17:13:06 +0400

    ispravlenie zadaniya_1

--- src/part2/lesson05/task01/Main.java
+++ src/part2/lesson05/task01/Main.java
@@ -8,11 +8,16 @@ public class Main {
     int [] a=new int[] {1,2,3,4};
     for (int i=0;i<a.length;i++)
         System.out.println(a[i]);
-    a[0]=4;
+    /*a[0]=4;
     a[1]=3;
     a[2]=2;
     a[3]=1;
-        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(a));*/
+    for (int i= a.length-1;i>=0;i--)
+        System.out.print(a[i]);
+        //System.out.println(Arrays.toString(a));
+        //System.out.print(a[i]);
+
 
 
     }
